930662489e2052cea66c57cdae240b4e71b69ff5
root <root@LilRyuko.localdomain>
2019-05-06 00:03:28 -0400

    Guts is operational

---
+++ guts.js
@@ -0,0 +1,19 @@
+const Discord = require('discord.js')
+const client = new Discord.Client()
+const ytdl = require('ytdl-core')
+const streamOptions = { seek: 0, volume: 1  }
+const broadcast = client.createVoiceBroadcast()
+
+client.on('ready', () => {
+  const voiceChannel = client.channels.get('421347180792512517')
+  if (!voiceChannel) return console.error('The channel does not exist')
+  voiceChannel.join()
+    .then(connection => {
+      const stream = ytdl('https://www.youtube.com/watch?v=ocQ6PDiP014', { fileter : 'audioonly' })
+      broadcast.playStream(stream)
+      const dispatcher = connection.playBroadcast(broadcast)
+    })
+    .catch(console.error)
+})
+
+client.login("NTc0Nzk3NTE1NDg0ODIzNTUy.XM-oKg.nWKUK0XVHy_FhQg9nZL_SODT-m0")
