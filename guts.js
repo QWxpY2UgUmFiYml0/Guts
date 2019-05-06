b9e041ba8291e14ed259ad0a8d5174105a1e6d2a
root <root@LilRyuko.localdomain>
2019-05-06 00:49:19 -0400

    guts is now summoned on command

--- guts.js
+++ guts.js
@@ -5,15 +5,25 @@ const streamOptions = { seek: 0, volume: 1  }
 const broadcast = client.createVoiceBroadcast()
 
 client.on('ready', () => {
-  const voiceChannel = client.channels.get('421347180792512517')
-  if (!voiceChannel) return console.error('The channel does not exist')
-  voiceChannel.join()
-    .then(connection => {
-      const stream = ytdl('https://www.youtube.com/watch?v=ocQ6PDiP014', { fileter : 'audioonly' })
-      broadcast.playStream(stream)
-      const dispatcher = connection.playBroadcast(broadcast)
-    })
-    .catch(console.error)
+})
+
+client.on('message', (recievedMessage) => {
+  if (recievedMessage.author == client.user) {
+    return
+  }
+
+  if (recievedMessage.content.includes(client.user.toString())) {
+    const voiceChannel = client.channels.get('421347180792512517')
+    if (!voiceChannel) return console.error('The channel does not exist')
+    voiceChannel.join()
+      .then(connection => {
+        const stream = ytdl('https://www.youtube.com/watch?v=ocQ6PDiP014', { fileter : 'audioonly' })
+        broadcast.playStream(stream)
+        const dispatcher = connection.playBroadcast(broadcast)
+      })
+      .catch(console.error)
+  }
 })
 
 client.login("NTc0Nzk3NTE1NDg0ODIzNTUy.XM-oKg.nWKUK0XVHy_FhQg9nZL_SODT-m0")
+
