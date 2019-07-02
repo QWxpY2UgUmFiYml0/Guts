e3a2bb9a8157145ccff83c668e4f378db12afa87
Ubuntu <ubuntu@ip-172-31-43-49.us-east-2.compute.internal>
2019-07-02 03:09:24 +0000

    guts should run forever now and has some cool grasses

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 ```
 #God help us all
+
+##Notes
+Bot key is exported
+Channel id is hard coded (refactor later)
+Youtube link is hard coded (refactor later)
 ```
--- guts.js
+++ guts.js
@@ -11,13 +11,15 @@ client.on('message', (recievedMessage) => {
   if (recievedMessage.author == client.user) {
     return
   }
-
+//  if (recievedMessage.content.includes('show me dem grasses')) {
+//    message.channel.send('ok', {files: ['./Content/grasses.jpg']})
+//  }
   if (recievedMessage.content.includes(client.user.toString())) {
     const voiceChannel = client.channels.get('421347180792512517')
     if (!voiceChannel) return console.error('The channel does not exist')
     voiceChannel.join()
       .then(connection => {
-        const stream = ytdl('https://www.youtube.com/watch?v=ocQ6PDiP014', { fileter : 'audioonly' })
+        const stream = ytdl('https://www.youtube.com/watch?v=ocQ6PDiP014', { filter : 'audioonly' })
         broadcast.playStream(stream)
         const dispatcher = connection.playBroadcast(broadcast)
       })
@@ -26,4 +28,3 @@ client.on('message', (recievedMessage) => {
 })
 
 client.login(process.env.BOT_TOKEN)
-
