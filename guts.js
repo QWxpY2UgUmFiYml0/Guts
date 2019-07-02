ecfa4b18eaef5a03f7681dec95aae51da248f59a
Ubuntu <ubuntu@ip-172-31-43-49.us-east-2.compute.internal>
2019-07-02 05:33:12 +0000

    made guts bot better

--- guts.js
+++ guts.js
@@ -1,24 +1,27 @@
-const Discord = require('discord.js')
-const client = new Discord.Client()
+const { Client, Attachment } = require('discord.js')
+
+//new instance of a discord client
+const client = new Client()
+
+//stuff for playing yt vids
 const ytdl = require('ytdl-core')
 const streamOptions = { seek: 0, volume: 1  }
 const broadcast = client.createVoiceBroadcast()
 
+
 client.on('ready', () => {
+  console.log('GRIFFIFFF')
 })
 
-client.on('message', (recievedMessage) => {
-  if (recievedMessage.author == client.user) {
-    return
-  }
-  client.on('message', message => {
-    if (message.content === 'show me dem grasses') {
-     const attachment = new Attachment('./Content/grasses.jpg')
-     message.channel.send(attachment)
-    }
-  })
+client.on('message', message => {
+if (message.content === 'show me dem grasses') {
+const attachment = new Attachment('./Content/grasses.jpg')
+message.channel.send(attachment)
+}
+})
 
-  if (recievedMessage.content.includes(client.user.toString())) {
+client.on('message', message => {
+if (message.content === '!grasses') {
     const voiceChannel = client.channels.get('421347180792512517')
     if (!voiceChannel) return console.error('The channel does not exist')
     voiceChannel.join()
@@ -28,7 +31,13 @@ client.on('message', (recievedMessage) => {
         const dispatcher = connection.playBroadcast(broadcast)
       })
       .catch(console.error)
-  }
+}
+})
+
+client.on('message', message => {
+if (message.content === '!help') {
+	message.channel.send('!grasses - play berserk op' + '\n' + 'show me dem grasses - display guts\' profile pic')
+}
 })
 
 client.login(process.env.BOT_TOKEN)
